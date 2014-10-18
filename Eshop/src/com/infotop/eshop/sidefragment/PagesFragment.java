ffe71fedcf6d5a3ffbe194628f662138402ca8eb
Pabitra <pabitranou1@gmail.com>
2014-10-18 17:03:12 +0800

    I have added comment on page fragment

--- Eshop/src/com/infotop/eshop/sidefragment/PagesFragment.java
+++ Eshop/src/com/infotop/eshop/sidefragment/PagesFragment.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+//Page Fragment
 public class PagesFragment extends Fragment {
 	
 	public PagesFragment(){}
