d9714f0a5ed3ccd6dae336536d12e876d01287fb
Balchender <balchender.nakkala@gmail.com>
2014-10-20 10:12:15 +0800

    I have created Registration page with validations in Eshop

--- Eshop/src/com/infotop/eshop/activities/RegisterActivity.java
+++ Eshop/src/com/infotop/eshop/activities/RegisterActivity.java
@@ -6,16 +6,63 @@ import com.infotop.eshop.R.layout;
 import com.infotop.eshop.R.menu;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends Activity {
+
+	private EditText userName;
+	private EditText userEmail;
+	private EditText userPwd;
+	private EditText userCPwd;
+	private EditText userBAdd;
+	private EditText userSAdd;
+	private EditText userMobile;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
+	}
+
+	public void getRegisterPage(View view) {
+		userName = ((EditText) findViewById(R.id.RuserName));
+		userEmail = ((EditText) findViewById(R.id.RuserEmail));
+		userPwd = ((EditText) findViewById(R.id.Rpassword));
+		userCPwd = ((EditText) findViewById(R.id.Rcpassword));
+		userBAdd = ((EditText) findViewById(R.id.Rbaddress));
+		userSAdd = ((EditText) findViewById(R.id.Rsaddress));
+		userMobile = ((EditText) findViewById(R.id.Rmobile));
+		if (userName.getText().toString().trim().equals("")) {
+			userName.setError("User name is required!");
+		} else if (userEmail.getText().toString().trim().equals("")) {
+			userEmail.setError("Email is required!");
+		} else if (userPwd.getText().toString().trim().equals("")) {
+			userPwd.setError("password is required!");
+		} else if (userCPwd.getText().toString().trim().equals("")) {
+			userCPwd.setError("Confirm password is required!");
+		} else if (userBAdd.getText().toString().trim().equals("")) {
+			userBAdd.setError("Billing Address is required!");
+		} else if (userSAdd.getText().toString().trim().equals("")) {
+			userSAdd.setError("Shipping Address is required!");
+		} else if (userMobile.getText().toString().trim().equals("")) {
+			userMobile.setError("Mobile Number is required!");
+		} else if (userPwd.getText().toString()
+				.equals(userCPwd.getText().toString())) {
+			Toast.makeText(getApplicationContext(), "Successfully Registered",
+					Toast.LENGTH_SHORT).show();
+			Intent intent = new Intent(this, EshopLoginActivity.class);
+			startActivity(intent);
+
+		} else {
+			Toast.makeText(getApplicationContext(),
+					"Both paswords should be same", Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	@Override
