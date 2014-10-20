c4fee299d2a9a361132cec51dd1f96231aeb56a2
Balchender <balchender.nakkala@gmail.com>
2014-10-20 10:58:40 +0800

    I have modified in login page and string xml file

--- Eshop/src/com/infotop/eshop/activities/EshopLoginActivity.java
+++ Eshop/src/com/infotop/eshop/activities/EshopLoginActivity.java
@@ -17,34 +17,39 @@ import android.widget.Toast;
 
 public class EshopLoginActivity extends Activity {
 
+	private EditText userEmail;
+	private EditText password;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_eshop_login);	
+		setContentView(R.layout.activity_eshop_login);
 	}
 
 	public void getHomePage(View view) {
-		EditText editText = (EditText) findViewById(R.id.editText1);
-		EditText editText1 = (EditText) findViewById(R.id.editText2);
-		//TextView textView = (TextView) findViewById(R.id.textView4);
-		String userName=editText.getText().toString();
-		String password=editText1.getText().toString();
-		if(userName.equals("iamtop")&&password.equals("1234")){
+		userEmail = (EditText) findViewById(R.id.editText1);
+		password = (EditText) findViewById(R.id.editText2);
+		if (userEmail.getText().toString().trim().equals("")) {
+			userEmail.setError("Email is required!");
+		} else if (password.getText().toString().trim().equals("")) {
+			password.setError("Password is required!");
+		}
+		else if (userEmail.getText().toString().equals("abc@info.com")
+				&& password.getText().toString().equals("1234")) {
 			Intent intent = new Intent(this, EshopMainActivity.class);
 			startActivity(intent);
-		}
-		else{
+		} else {
 			Toast.makeText(getApplicationContext(), "Wrong Credentials",
-				      Toast.LENGTH_SHORT).show();
-			//textView.setText("wrong credentials");
+					Toast.LENGTH_SHORT).show();
+			// textView.setText("wrong credentials");
 		}
 	}
-	
+
 	public void signUp(View view) {
 		Intent intent = new Intent(this, RegisterActivity.class);
 		startActivity(intent);
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
