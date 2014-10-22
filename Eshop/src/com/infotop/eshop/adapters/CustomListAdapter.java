4a66d8fa84de364f2f411f597818c323286c0330
Pabitra <pabitranou1@gmail.com>
2014-10-22 11:18:48 +0800

    I have added Author and Price to the main List view of Books

--- Eshop/src/com/infotop/eshop/adapters/CustomListAdapter.java
+++ Eshop/src/com/infotop/eshop/adapters/CustomListAdapter.java
@@ -19,13 +19,18 @@ public class CustomListAdapter extends ArrayAdapter<String>{
 	private final Activity context;
     private final String[] web;
     private final Integer[] imageId;
+    private final String[] author;
+    private final String[] price;
+    
 
-    public CustomListAdapter(Activity context,String[] web, Integer[] imageId) {
+    public CustomListAdapter(Activity context,String[] web, Integer[] imageId,String[] author,String[] price) {
 
     	super(context, R.layout.custom_list, web);
     	this.context = context;
     	this.web = web;
     	this.imageId = imageId;
+    	this.author=author;
+    	this.price=price;
 }
 
 @SuppressLint({ "ViewHolder", "InflateParams" })
@@ -34,9 +39,14 @@ public View getView(int position, View view, ViewGroup parent) {
 
 	LayoutInflater inflater = context.getLayoutInflater();
 	View rowView= inflater.inflate(R.layout.custom_list, null, true);
-	TextView txtTitle = (TextView) rowView.findViewById(R.id.txt);
+	TextView txtTitle = (TextView) rowView.findViewById(R.id.books_name);
+	TextView txtAuthor=(TextView) rowView.findViewById(R.id.books_author);
+	TextView txtPrice=(TextView) rowView.findViewById(R.id.books_price);
 	ImageView imageView = (ImageView) rowView.findViewById(R.id.img);
+	
 	txtTitle.setText(web[position]);
+	txtAuthor.setText(author[position]);
+	txtPrice.setText(price[position]);
 	imageView.setImageResource(imageId[position]);
 	
 	return rowView;
--- Eshop/src/com/infotop/eshop/sidefragment/BooksFragment.java
+++ Eshop/src/com/infotop/eshop/sidefragment/BooksFragment.java
@@ -35,6 +35,24 @@ public class BooksFragment extends Fragment {
 			R.drawable.ic_book21, R.drawable.ic_book22, R.drawable.ic_book23,
 			R.drawable.ic_book24, R.drawable.ic_book25, R.drawable.ic_book26,
 			R.drawable.ic_book27, R.drawable.ic_book28, R.drawable.ic_book29 };
+	
+    String[] author={"Author:Pabitra","Author:Balchander","Author: S.K Dutta",
+    		"Author: Devid","Author: Dephenar L","Author: Rajesh P","Author: Rakesh Menon",
+    		"Author: Snthil Kumar","Author: PrabaKaran","Author: Rajesh","Author: Arun K.",
+    		"Author:Tian Xiangly","Author: Mrs Leo","Author: Chian Yang","Author: Sudeep ",
+    		"Author: Summy Darien","Author:Petor Burg","Author: L.Kluzner","Author: P.Sepherd",
+    		"Author: Sakil","Author: L.M Nahama","Author: S.Babu","Author: N Murthy","Author: S.pati",
+    		"Author: Z.Nelson","Author: P.S Panda","Author: J K Mantri","Author: J.M Tripathy",
+    		"Author: R.K Burma"};
+    
+    String[] price={"Price: $102","Price: $110","Price: $40","Price: $65","Price: $73","Price: $86",
+			"Price: $89","Price: $320","Price: $326","Price: $560","Price: $143","Price: $150",
+			"Price: $120","Price: $430","Price: $210","Price: $540","Price: $15","Price: $45",
+			"Price: $42","Price: $75","Price: $65","Price: $143","Price: $320","Price: $70",
+			"Price: $156","Price: $436","Price: $210","Price: $301","Price: $430"};
+	
+	
+	
 	CustomListAdapter adapter;
 
 	public BooksFragment() {
@@ -47,7 +65,7 @@ public class BooksFragment extends Fragment {
 		View rootView = inflater.inflate(
 				R.layout.activity_books_list_view, container, false);
 		// Custom Adapter object
-		adapter = new CustomListAdapter(getActivity(), web, imageId);
+		adapter = new CustomListAdapter(getActivity(), web, imageId,author,price);
 		// Adapter Object set to a list
 		list = (ListView) rootView.findViewById(R.id.list);
 		list.setAdapter(adapter);
@@ -73,3 +91,4 @@ public class BooksFragment extends Fragment {
 	}
 
 }
+
