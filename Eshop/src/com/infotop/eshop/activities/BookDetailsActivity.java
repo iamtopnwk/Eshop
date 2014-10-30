197cdc62ba239c6b724348cd365d78b3a4fe1619
Balchender <balchender.nakkala@gmail.com>
2014-10-30 11:19:12 +0800

    I have stored wishlist and cartlist items into sqllitedb

--- Eshop/src/com/infotop/eshop/activities/BookDetailsActivity.java
+++ Eshop/src/com/infotop/eshop/activities/BookDetailsActivity.java
@@ -5,11 +5,15 @@
 package com.infotop.eshop.activities;
 
 //import com.infotop.eshop.activities.ProductDetailsHorizontalActivity;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import com.infotop.eshop.R;
 import com.infotop.eshop.adapters.CustomListHorizontalAdapter;
 import com.infotop.eshop.adapters.HorizontalListView;
+import com.infotop.eshop.db.DatabaseHandler;
+import com.infotop.eshop.model.Wishlist;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -37,14 +41,14 @@ public class BookDetailsActivity extends Activity {
 	ImageButton cartBtn1;
 	ImageButton buyBtn1;
 	ImageButton wishlistBtn1;
+	ArrayList<String> s;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_book_details);
 		// Get data from EshopMainActivity
-		ArrayList<String> s = getIntent().getExtras().getStringArrayList(
-				"productData");
+		s = getIntent().getExtras().getStringArrayList("productData");
 		System.out.println("Product Name:" + s.get(0));
 		TextView tv = (TextView) findViewById(R.id.bookName1);
 		TextView tv1 = (TextView) findViewById(R.id.authorName);
@@ -60,7 +64,14 @@ public class BookDetailsActivity extends Activity {
 	// functionalities for cartBtn
 	public void addToCart(View view) {
 		System.out.println("Add Cart Button");
-
+		DatabaseHandler db = new DatabaseHandler(BookDetailsActivity.this);
+		Wishlist w = new Wishlist();
+		w.setProductId(s.get(0));
+		w.setProductName(s.get(1));
+		w.setDescription(s.get(2));
+		w.setPrice(s.get(3));
+		w.setCreatedDate(new SimpleDateFormat("dd MMM yyyy").format(new Date()));
+		db.addCartList(w);
 		Toast.makeText(BookDetailsActivity.this, "Your Item is Added to Cart",
 				Toast.LENGTH_SHORT).show();
 
@@ -78,6 +89,16 @@ public class BookDetailsActivity extends Activity {
 
 	// functionalities for wishlistBtn
 	public void addToWishlist(View view) {
+
+		DatabaseHandler db = new DatabaseHandler(BookDetailsActivity.this);
+		Wishlist w = new Wishlist();
+		w.setProductId(s.get(0));
+		w.setProductName(s.get(1));
+		w.setDescription(s.get(2));
+		w.setPrice(s.get(3));
+		w.setCreatedDate(new SimpleDateFormat("dd MMM yyyy").format(new Date()));
+
+		db.addWishList(w);
 		System.out.println("Add WishList Button");
 
 		Toast.makeText(BookDetailsActivity.this,
---
+++ Eshop/src/com/infotop/eshop/model/Wishlist.java
@@ -0,0 +1,74 @@
+package com.infotop.eshop.model;
+
+public class Wishlist {
+
+	private int id;
+	private String productId;
+	private String productName;
+	private String description;
+	private String price;
+	private String categoryId;
+	public String getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	private String createdDate;
+
+	public Wishlist() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProductId() {
+		return productId;
+	}
+
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(String createdDate) {
+		this.createdDate = createdDate;
+	}
+
+}
