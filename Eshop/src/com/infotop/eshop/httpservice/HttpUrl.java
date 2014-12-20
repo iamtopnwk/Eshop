272c35876c18267124ac73aa37eab8017f536aca
Balchender <balchender.nakkala@gmail.com>
2014-12-20 09:39:05 +0800

    I have changed ip

--- Eshop/src/com/infotop/eshop/httpservice/HttpUrl.java
+++ Eshop/src/com/infotop/eshop/httpservice/HttpUrl.java
@@ -3,7 +3,7 @@ package com.infotop.eshop.httpservice;
 public class HttpUrl {
 
 	public static String getUrl(){
-		String url="http://192.168.8.162";
+		String url="http://192.168.8.163";
 		return url;
 	}
 }
